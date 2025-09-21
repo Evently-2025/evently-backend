d3f5243b2c1f73f47687484784e58b657fce692a
Rashmi-Wijesekara <rashmiwijesekara1999@gmail.com>
2025-09-21 14:25:25 +0530

    refactor: to clean architecture

---
+++ src/Modules/Events/Evently.Modules.Events.Domain/Events/Event.cs
@@ -0,0 +1,37 @@
+﻿using Evently.Modules.Events.Domain.Abstractions;
+
+namespace Evently.Modules.Events.Domain.Events;
+
+public sealed class Event: Entity
+{
+    private Event() { }
+
+    public Guid Id { get; private set; }
+    public string Title { get; private set; }
+    public string Description { get; private set; }
+    public string Location { get; private set; }
+    public DateTime StartAtUtc { get; private set; }
+    public DateTime? EndAtUtc { get; private set; }
+    public EventStatus Status { get; private set; }
+
+    public static Event Create(
+        string Title,
+        string Description,
+        string Location,
+        DateTime StartAtUtc,
+        DateTime? EndAtUtc)
+    {
+        var @event = new Event
+        {
+            Id = Guid.NewGuid(),
+            Title = Title,
+            Description = Description,
+            Location = Location,
+            StartAtUtc = StartAtUtc,
+            EndAtUtc = EndAtUtc
+        };
+
+        @event.Raise(new EventCreatedDomainEvent(@event.Id));
+        return @event;
+    }
+}
--- src/Modules/Events/Evently.Modules.Events.Infrastructure/EventsModule.cs
+++ src/Modules/Events/Evently.Modules.Events.Infrastructure/EventsModule.cs
@@ -1,28 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Evently.Modules.Events.Api.Database;
-using Evently.Modules.Events.Api.Events;
+﻿using Evently.Modules.Events.Application.Abstractions.Data;
+using Evently.Modules.Events.Domain.Events;
+using Evently.Modules.Events.Infrastructure.Data;
+using Evently.Modules.Events.Infrastructure.Database;
+using Evently.Modules.Events.Infrastructure.Events;
+using Evently.Modules.Events.Presentation.Events;
+using FluentValidation;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Npgsql;
 
-namespace Evently.Modules.Events.Api;
+namespace Evently.Modules.Events.Infrastructure;
 public static class EventsModule
 {
     public static void MapEndpoints(IEndpointRouteBuilder app)
     {
-        CreateEvent.MapEndpoint(app);
-        GetEvent.MapEndpoint(app);
+        EventEndpoints.MapEndpoints(app);
     }
 
     public static IServiceCollection AddEventsModule(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Application.AssemblyReference.Assembly);
+        });
+        services.AddValidatorsFromAssembly(Application.AssemblyReference.Assembly, includeInternalTypes: true);
+
+        services.AddInfrastructure(configuration);
+        return services;
+    }
+
+    private static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
         string databaseConnectionString = configuration.GetConnectionString("Database");
+
+        NpgsqlDataSource npgsqlDataSource = new NpgsqlDataSourceBuilder(databaseConnectionString).Build();
+        services.TryAddSingleton(npgsqlDataSource);
+
+        services.AddScoped<IDbConnectionFactory, DbConnectionFactory>();
 
         services.AddDbContext<EventsDbContext>(options =>
             options.UseNpgsql(
@@ -42,7 +60,8 @@ public static class EventsModule
                 })
             .UseSnakeCaseNamingConvention());
 
-        return services;
-    }
+        services.AddScoped<IEventRepository, EventRepository>();
 
+        services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<EventsDbContext>());
+    }
 }
